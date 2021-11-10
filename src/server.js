97759fecffd71821dacf156f1ff9d11016627238
haemiii <73984822+haemiii@users.noreply.github.com>
2021-11-10 16:40:43 +0900

    server basic2-middleware

--- src/server.js
+++ src/server.js
@@ -6,13 +6,35 @@ const app = express(); //server = application 만들기
 //server is always conneted with internet
 //type google.com -> request, server is always listening
 
+//middleware -> between request and respond :dosen't respond, expand request
+//middleware(call next function) <= controller 
+const loggerMiddleware = (req ,res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+const privateMiddleware = (req, res, next) =>{
+    const url = req.url;
+    if(url == "/protected"){
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    next();
+};
 
+const handleProtected = (req, res) =>{
+    return res.send("Welcome to the private lounge.");
+};
+
+//globalMiddleware : using every route
+app.use(loggerMiddleware); 
+app.use(privateMiddleware);
 //application setting
-
-app.get("/",(req, res)=> {
+//app.get-> the way:server respond
+app.get("/", (req, res)=> {// create route, callback : controller
     console.log(req);
     //return res.end, return res.send("hi!!!!")
-    }); // route, callback
+    }); 
+app.get("/protected", handleProtected);
+
 
 const handleLogin  = (req, res) =>{
     return res.send("Login here!!");
