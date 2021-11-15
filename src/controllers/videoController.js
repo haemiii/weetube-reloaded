e0477eccd9d2599079ed98d72069b410d22d0a57
haemiii <73984822+haemiii@users.noreply.github.com>
2021-11-15 10:21:55 +0900

    templates-pug2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,31 @@
 export const trending = (req, res)=> {
-    res.render("home");
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ]
+    return res.render("home", {pageTitle : "Home", videos});
 }
 
 export const see = (req, res) => {
@@ -7,7 +33,7 @@ export const see = (req, res) => {
     return res.render("watch");
 }
 export const edit = (req, res) => {
-    return res.send("Edit Video");
+    return res.render("edit");
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
