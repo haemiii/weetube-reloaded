1f1254ff835b14a3950f207edc094ea4efc78db7
haemiii <73984822+haemiii@users.noreply.github.com>
2021-11-29 23:21:54 +0900

    User register6 github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
 export const getJoin = (req, res)=> {
     res.render("join",{pageTitle: "Join"});
@@ -47,7 +48,7 @@ export const getLogin = (req, res) => {
     res.render("login", {pageTitle: "Login"})};
 export const postLogin =async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username ,socialOnly : false});
     if(!user){
         return res.status(400).render("login",{ 
             pageTitle: "Login", 
@@ -75,6 +76,7 @@ export const startGithubLogin = (req,res) =>{
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `https://github.com/login/oauth/authorize?${params}`;
+    return res.redirect(finalUrl);
 };
 
 export const finishGithubLogin = async(req, res) => {
@@ -86,12 +88,59 @@ export const finishGithubLogin = async(req, res) => {
     };
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const data = await fetch(finalUrl, {
-        method:"POST"
-    })
-    const json = await data.json();
+    const tokenRequest = await(
+        await fetch(finalUrl, {
+            method:"POST",
+            headers : {
+                Accept: "application/json"
+            }
+        })
+    ).json();
+    if("access_token" in tokenRequest){
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com"
+        const userData = await( 
+            await fetch(`${apiUrl}/user`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        const emailData = await( 
+            await fetch(`${apiUrl}/user/emails`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if(!emailObj) {
+            return res.redirect("/login");
+        }
+        let user = await User.findOne({email : emailObj.email});
+        if(!user){
+            user = await User.create({
+                avartarUrl:userData.avatar_url,
+                name:userData.name ? userData.name: "Unknown",
+                username:userData.login,
+                email:emailObj.email,
+                password:"",
+                socialOnly: true,
+                location:userData.location,
+            });
+        }
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");
+    }else{
+        return res.redirect("/login");
+    }
+};
+export const logout = (req, res) => {
+    req.session.destory();
+    return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit user");
-export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";  //node_modules에서 찾아줌!!
-import {edit, remove, see, logout, startGithubLogin, finishGithubLogin} from "../controllers/userController";
+import {edit, see, logout, startGithubLogin, finishGithubLogin} from "../controllers/userController";
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove",remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 
